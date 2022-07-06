655f245cfca504339d7c99fbee985e70451c1956
Bynood <quent1_jarry@hotmail.com>
2022-07-06 22:14:24 +0200

    clean extra files

--- gendergap.js
+++ gendergap.js
@@ -99,7 +99,7 @@ const Pays = [
         },
         {
             "id": "LTU",
-            "name": "Lithuanie"
+            "name": "Lituanie"
         },
         {
             "id": "ESP",
@@ -208,25 +208,6 @@ const Pays = [
             "name": "Australie"        }
     ]
 
-
-// fetch('https://stats.oecd.org/SDMX-JSON/data/DEC_I/AUS+AUT+BEL+CAN+CHL+COL+CRI+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+OECD+BGR+HRV+CYP+MLT+ROU.MEN+WOMEN+MW.GWG5/all?startTime=2005&endTime=2020&dimensionAtObservation=allDimensions')
-// .then( (data) => {
-//     if(data.ok){
-//         return data.json()
-        
-//     } 
-// })
-// .then( gendergap => generateHtml(gendergap))
-// .catch( error => console.error('Error:', error))
-
-// const generateHtml = (data) => {
-//     console.log(data)
-//     const html = `
-//         <div class="name">${data}</div>
-//     `
-//     const gendergapDiv = document.querySelector('.gendergap')
-//     gendergapDiv.innerHTML = html
-// }
 
 
 function countryObservation(d){
